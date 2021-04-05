584d061614256ef0717a7238eabf1cbb1faa3d22
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2021-04-06 00:54:43 +0530

    4 bit add sub
    added

---
+++ 4 bit add sub/4_bit_Add_Sub/Sources/Adder_subtractor_unit.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/31/2021 01:50:26 PM
+-- Design Name: 
+-- Module Name: Adder_subtractor_unit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Adder_subtractor_unit is
+--  Port ( );
+end Adder_subtractor_unit;
+
+architecture Behavioral of Adder_subtractor_unit is
+
+begin
+
+
+end Behavioral;
