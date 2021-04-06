476c96fe04fed140b1de8df843ce06fb2e2db0d9
Sathira443 <liyanapathiranasathira@gmail.com>
2021-04-07 00:03:19 +0530

    Codes of MUX Changed

--- 8 way 4-bit MUX/8 way 4-bit MUX.srcs/sources_1/new/MUX_8_way_4_bit.vhd
+++ 8 way 4-bit MUX/8 way 4-bit MUX.srcs/sources_1/new/MUX_8_way_4_bit.vhd
@@ -1,71 +1,81 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity MUX_8_way_4_bit is
-    Port ( Reg0 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg1 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg2 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg3 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg4 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg5 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg6 : in STD_LOGIC_VECTOR (3 downto 0);
-           Reg7 : in STD_LOGIC_VECTOR (3 downto 0);
-           RegValue : out STD_LOGIC_VECTOR (3 downto 0);
-           RegSelection : in STD_LOGIC_VECTOR (2 downto 0));
-end MUX_8_way_4_bit;
+ENTITY MUX_8_way_4_bit IS
+    PORT (
+        Reg0 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg1 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg2 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg3 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg4 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg5 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg6 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Reg7 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        RegValue : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        RegSelection : IN STD_LOGIC_VECTOR (2 DOWNTO 0));
+END MUX_8_way_4_bit;
 
-architecture Behavioral of MUX_8_way_4_bit is
-Component Decoder_3_to_8
-    port(   I : in STD_LOGIC_VECTOR (2 downto 0);
-            EN : in STD_LOGIC;
-            Y : out STD_LOGIC_VECTOR (7 downto 0));
- end component;
+ARCHITECTURE Behavioral OF MUX_8_way_4_bit IS
+    COMPONENT Decoder_3_to_8
+        PORT (
+            I : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+            EN : IN STD_LOGIC;
+            Y : OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
+    END COMPONENT;
 
-Signal YY : std_logic_vector(7 downto 0);
+    SIGNAL YY : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
-component tri_state_buffer
-    port(  inputTri : in STD_LOGIC_VECTOR (3 downto 0);
-           outputTri : out STD_LOGIC_VECTOR (3 downto 0);
-           EN : in STD_LOGIC);
-end component;
-begin
+    COMPONENT tri_state_buffer
+        PORT (
+            inputTri : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            outputTri : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            EN : IN STD_LOGIC);
+    END COMPONENT;
+BEGIN
 
-Decoder_3_to_8_0 : Decoder_3_to_8
-    port map (  I => RegSelection,
-            EN => '1',
-            Y => YY);
-tri_state_buffer_0 : tri_state_buffer
-    port map(   inputTri => Reg0,
-                EN=> YY(0),
-                outputTri => RegValue);
-tri_state_buffer_1 : tri_state_buffer
-    port map(   inputTri => Reg1,
-                EN=> YY(1),
-                outputTri => RegValue);
-tri_state_buffer_2 : tri_state_buffer
-    port map(   inputTri => Reg2,
-                EN=> YY(2),
-                outputTri => RegValue);
-tri_state_buffer_3 : tri_state_buffer
-    port map(   inputTri => Reg3,
-                EN=> YY(3),
-                outputTri => RegValue);
-tri_state_buffer_4 : tri_state_buffer
-    port map(   inputTri => Reg4,
-                EN=> YY(4),
-                outputTri => RegValue);
-tri_state_buffer_5 : tri_state_buffer
-    port map(   inputTri => Reg5,
-                EN=> YY(5),
-                outputTri => RegValue);
-tri_state_buffer_6 : tri_state_buffer
-    port map(   inputTri => Reg6,
-                EN=> YY(6),
-                outputTri => RegValue);
-tri_state_buffer_7 : tri_state_buffer
-    port map(   inputTri => Reg7,
-                EN=> YY(7),
-                outputTri => RegValue);                
-
-
-end Behavioral;
+    Decoder_3_to_8_0 : Decoder_3_to_8
+    PORT MAP(
+        I => RegSelection,
+        EN => '1',
+        Y => YY);
+    tri_state_buffer_0 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg0,
+        EN => YY(0),
+        outputTri => RegValue);
+    tri_state_buffer_1 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg1,
+        EN => YY(1),
+        outputTri => RegValue);
+    tri_state_buffer_2 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg2,
+        EN => YY(2),
+        outputTri => RegValue);
+    tri_state_buffer_3 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg3,
+        EN => YY(3),
+        outputTri => RegValue);
+    tri_state_buffer_4 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg4,
+        EN => YY(4),
+        outputTri => RegValue);
+    tri_state_buffer_5 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg5,
+        EN => YY(5),
+        outputTri => RegValue);
+    tri_state_buffer_6 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg6,
+        EN => YY(6),
+        outputTri => RegValue);
+    tri_state_buffer_7 : tri_state_buffer
+    PORT MAP(
+        inputTri => Reg7,
+        EN => YY(7),
+        outputTri => RegValue);
+END Behavioral;
--- RegisterBank/RegisterBank/RegisterBank.srcs/sources_1/new/RegisterBank.vhd
+++ RegisterBank/RegisterBank/RegisterBank.srcs/sources_1/new/RegisterBank.vhd
@@ -1,131 +1,129 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity RegisterBank is
-    Port (
-        D : in STD_LOGIC_VECTOR (3 downto 0);
-        I : in STD_LOGIC_VECTOR (2 downto 0);
-        Clk : in STD_LOGIC;
-        Enable : in STD_LOGIC;
-        Res : in STD_LOGIC;
-        R0 : out STD_LOGIC_VECTOR (3 downto 0);
-        R1 : out STD_LOGIC_VECTOR (3 downto 0);
-        R2 : out STD_LOGIC_VECTOR (3 downto 0);
-        R3 : out STD_LOGIC_VECTOR (3 downto 0);
-        R4 : out STD_LOGIC_VECTOR (3 downto 0);
-        R5: out STD_LOGIC_VECTOR (3 downto 0);
-        R6 : out STD_LOGIC_VECTOR (3 downto 0);
-        R7 : out STD_LOGIC_VECTOR (3 downto 0);
-        R8 : out STD_LOGIC_VECTOR (3 downto 0));
-        
-        
-end RegisterBank;
+ENTITY RegisterBank IS
+    PORT (
+        D : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        I : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        Clk : IN STD_LOGIC;
+        Enable : IN STD_LOGIC;
+        Res : IN STD_LOGIC;
+        R0 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R1 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R2 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R3 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R4 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R5 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R6 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R7 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        R8 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0));
+END RegisterBank;
 
-architecture Behavioral of RegisterBank is
+ARCHITECTURE Behavioral OF RegisterBank IS
 
--- Add Registers to the Program counter
-component Reg
-Port (
-        D : in STD_LOGIC_VECTOR (3 downto 0);
-        Enable : in STD_LOGIC;
-        Clk : in STD_LOGIC;
-        Res : in STD_LOGIC;
-        Q : out STD_LOGIC_VECTOR (3 downto 0));
-end component;
+    -- Add Registers to the Program counter
+    COMPONENT Reg
+        PORT (
+            D : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            Enable : IN STD_LOGIC;
+            Clk : IN STD_LOGIC;
+            Res : IN STD_LOGIC;
+            Q : OUT STD_LOGIC_VECTOR (3 DOWNTO 0));
+    END COMPONENT;
 
--- Add Slow Clock to the program counter
-component Slow_Clock
-port(
-    Clk_In : in std_logic;
-    Clk_Out : out std_logic
-    );
-end component;
+    -- Add Slow Clock to the program counter
+    COMPONENT Slow_Clock
+        PORT (
+            Clk_In : IN STD_LOGIC;
+            Clk_Out : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
-component Decoder_3_to_8
-    PORT(
-        I : in std_logic_vector(2 downto 0);
-        Y : out std_logic_vector(7 downto 0);
-        Enable: in std_logic);
-end component;
+    COMPONENT Decoder_3_to_8
+        PORT (
+            I : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            Y : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            Enable : IN STD_LOGIC);
+    END COMPONENT;
 
-signal Clk_slow:std_logic;
-signal Y :  std_logic_vector(7 downto 0);
+    SIGNAL Clk_slow : STD_LOGIC;
+    SIGNAL Y : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
-begin
-    Decoder_3_to_8_0:Decoder_3_to_8
-    port map(
-        I=>I,
-        Enable=>Enable,
-        Y=>Y
+BEGIN
+    Decoder_3_to_8_0 : Decoder_3_to_8
+    PORT MAP(
+        I => I,
+        Enable => Enable,
+        Y => Y
     );
-    Slow_clock0:Slow_Clock
-    port map(
-        Clk_In=>Clk,
-        Clk_Out=>Clk_slow
+    Slow_clock0 : Slow_Clock
+    PORT MAP(
+        Clk_In => Clk,
+        Clk_Out => Clk_slow
     );
-    Reg0: Reg
-    port map(
-        D=>"0000",
-        Enable=>Y(0),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R0
+    Reg0 : Reg
+    PORT MAP(
+        D => "0000",
+        Enable => Y(0),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R0
     );
-    Reg1: Reg
-    port map(
-        D=>D,
-        Enable=>Y(1),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R1
+    Reg1 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(1),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R1
     );
-    Reg2: Reg
-    port map(
-        D=>D,
-        Enable=>Y(2),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R2
+    Reg2 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(2),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R2
     );
-    Reg3: Reg
-    port map(
-        D=>D,
-        Enable=>Y(3),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R3
+    Reg3 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(3),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R3
     );
-    Reg4: Reg
-    port map(
-        D=>D,
-        Enable=>Y(4),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R4
+    Reg4 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(4),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R4
     );
-    Reg5: Reg
-    port map(
-        D=>D,
-        Enable=>Y(5),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R5
+    Reg5 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(5),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R5
     );
-    Reg6: Reg
-    port map(
-        D=>D,
-        Enable=>Y(6),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R6
+    Reg6 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(6),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R6
     );
-    Reg7: Reg
-    port map(
-        D=>D,
-        Enable=>Y(7),
-        Clk=>Clk_slow,
-        Res=>Res,
-        Q=>R7
+    Reg7 : Reg
+    PORT MAP(
+        D => D,
+        Enable => Y(7),
+        Clk => Clk_slow,
+        Res => Res,
+        Q => R7
     );
 
-end Behavioral;
+END Behavioral;
