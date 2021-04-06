3e83b5a09bef79da4eeb55fdfd4bf31d237a6e3e
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2021-04-06 12:23:00 +0530

    Changes
    copied

--- Instruction Decoder/Source/InstructionDecoder.vhd
+++ Instruction Decoder/Source/InstructionDecoder.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/08/2018 01:52:36 PM
--- Design Name: 
--- Module Name: InstructionDecoder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -44,7 +23,6 @@ COMPONENT Decoder_2_to_4
 END COMPONENT;
  
 SIGNAL MOV, ADD, NEG, JZR: std_logic;
-SIGNAL ZERO : std_logic;
 SIGNAL JUMP_FLAG_0 : std_logic;
 
 begin
@@ -54,7 +32,7 @@ Decoder_2_to_40 : Decoder_2_to_4
     I(0) => X(10),
     I(1) => X(11),
     Y(0) => ADD,
-    Y(1) => SUB,
+    Y(1) => NEG,
     Y(2) => MOV,
     Y(3) => JZR,
     EN => '1');
@@ -66,8 +44,6 @@ REG_SEL_0 <= X(9 downto 7);
 REG_SEL_1 <= X(6 downto 4);
 SUB <= NEG;
 JUMP_TO <= X(2 downto 0);
-ZERO <= JUMP_CHECK;
-JUMP_FLAG_0 <= JZR AND ZERO;
-JUMP_FLAG <= JUMP_FLAG_0;
+JUMP_FLAG <= JZR AND (not(JUMP_CHECK));
 
 end Behavioral;
