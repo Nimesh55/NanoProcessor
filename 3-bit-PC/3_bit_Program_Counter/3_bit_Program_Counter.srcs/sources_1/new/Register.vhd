a4a8af1dc635cf0b2b90e5b26085ea61c0dbe400
Sathira443 <liyanapathiranasathira@gmail.com>
2021-04-06 23:30:46 +0530

    Eddited Comments

--- 3-bit-PC/3_bit_Program_Counter/3_bit_Program_Counter.srcs/sources_1/new/Register.vhd
+++ 3-bit-PC/3_bit_Program_Counter/3_bit_Program_Counter.srcs/sources_1/new/Register.vhd
@@ -1,72 +1,71 @@
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+ENTITY REGISTER IS
+    PORT (
+        D : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Res : IN STD_LOGIC;
+        Enable : IN STD_LOGIC;
+        Q : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Clk : IN STD_LOGIC);
+END REGISTER;
 
+ARCHITECTURE Behavioral OF REGISTER IS
+    -- Add D Flip Flops to the Program counter
+    COMPONENT D_FF
+        PORT (
+            D : IN STD_LOGIC;
+            Res : IN STD_LOGIC;
+            Clk : IN STD_LOGIC;
+            Q : OUT STD_LOGIC;
+            Qbar : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
-entity Register is
-    Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
-           Res : in STD_LOGIC;
-           Enable : in STD_LOGIC;
-           Q : out STD_LOGIC_VECTOR (3 downto 0);
-           Clk : in STD_LOGIC);
-end Register;
+    -- Add Slow Clock to the program counter
+    COMPONENT Slow_Clock
+        PORT (
+            Clk_In : IN STD_LOGIC;
+            Clk_Out : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
-architecture Behavioral of Register is
- -- Add D Flip Flops to the Program counter
- component D_FF
- port(
- D : in std_logic;
- Res:in std_logic;
- Clk:in std_logic;
- Q : out std_logic;
- Qbar : out std_logic  
- );
-end component;
+    SIGNAL D0, D1, D2 : STD_LOGIC;
+    SIGNAL Clk_slow : STD_LOGIC;
 
--- Add Slow Clock to the program counter
-component Slow_Clock
- port(
-     Clk_In : in std_logic;
-     Clk_Out : out std_logic
-     );
-end component;
+BEGIN
 
-signal D0,D1,D2 : std_logic;
-signal Clk_slow:std_logic;
+    D_FF0 : D_FF
+    PORT MAP(
+        D => D(0),
+        Res => Res,
+        Clk => Clk_slow,
+        Q => Q(0)
 
-begin
-
-    D_FF0: D_FF
-    port map(
-        D=>D(0),
-        Res=>Res,
-        Clk=>Clk_slow,
-        Q=>Q(0)
-        
     );
-    D_FF1: D_FF
-    port map(
-        D=>D(1),
-        Res=>Res,
-        Clk=>Clk_slow,
-        Q=>Q(1)
-        
+    D_FF1 : D_FF
+    PORT MAP(
+        D => D(1),
+        Res => Res,
+        Clk => Clk_slow,
+        Q => Q(1)
+
     );
-    D_FF2: D_FF
-    port map(
-        D=>D(2),
-        Res=>Res,
-        Clk=>Clk_slow,
-        Q=>Q(2)
-        
+    D_FF2 : D_FF
+    PORT MAP(
+        D => D(2),
+        Res => Res,
+        Clk => Clk_slow,
+        Q => Q(2)
+
     );
-    D_FF2: D_FF
-    port map(
-        D=>D(3),
-        Res=>Res,
-        Clk=>Clk_slow,
-        Q=>Q(3)
-        
+    D_FF2 : D_FF
+    PORT MAP(
+        D => D(3),
+        Res => Res,
+        Clk => Clk_slow,
+        Q => Q(3)
+
     );
 
-end Behavioral;
+END Behavioral;
