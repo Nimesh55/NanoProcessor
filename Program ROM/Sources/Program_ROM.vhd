8be2fc82a36011bd4f3382fb548e218e0f7a93a4
Sathira443 <liyanapathiranasathira@gmail.com>
2021-04-06 23:00:08 +0530

    Program ROM code eddited
    Edited the code

--- Program ROM/Sources/Program_ROM.vhd
+++ Program ROM/Sources/Program_ROM.vhd
@@ -1,28 +1,29 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity ProgramRom is
-    Port ( Memory_select  : in STD_LOGIC_VECTOR (2 downto 0); -- Memory selection 
-           Instruction : out STD_LOGIC_VECTOR (11 downto 0)); -- Instruction
-end ProgramRom;
+ENTITY ProgramRom IS
+    PORT (
+        Memory_select : IN STD_LOGIC_VECTOR (2 DOWNTO 0); -- Memory selection 
+        Instruction : OUT STD_LOGIC_VECTOR (11 DOWNTO 0)); -- Instruction
+END ProgramRom;
 
-architecture Behavioral of ProgramRom is
+ARCHITECTURE Behavioral OF ProgramRom IS
 
-type rom_type is array (0 to 7) of STD_LOGIC_VECTOR(11 downto 0);
+    TYPE rom_type IS ARRAY (0 TO 7) OF STD_LOGIC_VECTOR(11 DOWNTO 0);
 
-SIGNAL programRom:rom_type:=(
-    "101110000000",
-    "101100000011",
-    "101010001111",
-    "001111100000",
-    "001101010000",
-    "111100000111",
-    "110000000011",
-    "000000000000");
+    SIGNAL programRom : rom_type := (
+        "101110000000",
+        "101100000011",
+        "101010001111",
+        "001111100000",
+        "001101010000",
+        "111100000111",
+        "110000000011",
+        "000000000000");
 
-begin
+BEGIN
 
-Instruction<= programRom(to_integer(unsigned(Memory_select)));
+    Instruction <= programRom(to_integer(unsigned(Memory_select)));
 
-end Behavioral;
+END Behavioral;
